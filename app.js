525b82fe6686f23e129c758638a5c0a3bd112281
atakanhbk <atakanhbk@gmail.com>
2023-10-16 10:54:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let todoCreateForm = document.createElement("form");
 let inputText = document.createElement("input");
 let showInputText = document.createElement("button");
 let toDoListCardParent = document.createElement("div");
@@ -9,7 +10,7 @@ function CreateTask() {
   toDoListCard.style.height = "100px";
   toDoListCard.style.border = "2px solid black";
   toDoListCard.style.margin = "10px";
-  toDoListCard.innerHTML = "ATAKAN";
+  toDoListCard.innerHTML = `${inputText.value}`;
   toDoListCard.style.color = "white";
   toDoListCard.style.display = "flex";
   toDoListCard.style.alignItems = "center";
@@ -25,16 +26,24 @@ function ObjectStyleControl() {
   toDoListCardParent.style.gap = "10px";
 }
 
+function OnSubmitFunction(e) {
+  e.preventDefault();
+  console.log("On submit");
+}
+
 function AddEventListenerFunc() {
   showInputText.addEventListener("click", function () {
     CreateTask();
   });
+
+  todoCreateForm.addEventListener("submit",OnSubmitFunction)
 }
 
 function AppendChildFunc() {
-  document.body.appendChild(inputText);
-  document.body.appendChild(showInputText);
-  document.body.appendChild(toDoListCardParent);
+  document.body.appendChild(todoCreateForm);
+  todoCreateForm.appendChild(inputText);
+  todoCreateForm.appendChild(showInputText);
+  todoCreateForm.appendChild(toDoListCardParent);
 }
 
 ObjectStyleControl();

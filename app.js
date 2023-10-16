a1fd55d97b3aac58e5c1da1671445cbf11474419
atakanhbk <atakanhbk@gmail.com>
2023-10-16 12:45:13 +0300

    Add Delete Button

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ function ObjectStyleControl() {
   toDoListCardParent.style.display = "flex";
   toDoListCardParent.style.flexWrap = "wrap";
   toDoListCardParent.style.gap = "10px";
-  toDoListCardParent.className="todo-parent";
+  toDoListCardParent.className = "todo-parent";
 
   createToDo.type = "submit";
 
   clearAllTodo.innerHTML = "Clear All Todo";
   clearAllTodo.style.backgroundColor = "red";
-  clearAllTodo.type = "button"
+  clearAllTodo.type = "button";
 }
 
 function CreateTask(e) {
@@ -27,6 +27,8 @@ function CreateTask(e) {
   if (inputText.value === "") {
     alert("Please Enter Any Value");
   } else {
+    let deleteButton = document.createElement("button");
+    DeleteButtonStyle(deleteButton);
     let toDoListCard = document.createElement("div");
     let toDoListCardText = document.createElement("span");
 
@@ -34,6 +36,9 @@ function CreateTask(e) {
     ToDoListCardStyle(toDoListCard);
 
     toDoListCard.appendChild(toDoListCardText);
+    toDoListCard.appendChild(deleteButton);
+    deleteButton.addEventListener("click", DeleteToDo);
+   
     toDoListCardList.push(inputText.value);
     console.log(toDoListCardList);
     inputText.value = "";
@@ -45,6 +50,13 @@ function ClearAllList() {
     toDoListCardParent.removeChild(toDoListCardParent.lastChild);
     toDoListCardList = [];
   }
+}
+
+function DeleteToDo(e) {
+  const deleteButtonParent = e.target.parentElement;
+  const indexOfList = toDoListCardList.indexOf(deleteButtonParent.textContent);
+  toDoListCardList.splice(indexOfList, 1);
+  deleteButtonParent.remove();
 }
 
 function ToDoListCardStyle(card) {
@@ -60,26 +72,35 @@ function ToDoListCardStyle(card) {
   cardStyle.justifyContent = "center";
   cardStyle.backgroundColor = "white";
   cardStyle.color = "black";
-  cardStyle.wordBreak =  "break-word"; 
+  cardStyle.wordBreak = "break-word";
   cardStyle.overflow = "hidden";
   cardStyle.padding = "10px";
+  cardStyle.position = "relative";
 }
 
+function DeleteButtonStyle(object) {
+  const deleteButtonStyle = object.style;
+  deleteButtonStyle.width = "10px";
+  deleteButtonStyle.height = "10px";
+  deleteButtonStyle.position = "absolute";
+  deleteButtonStyle.top = "0";
+  deleteButtonStyle.right = "0";
+  object.type = "button";
+  console.log("");
+}
 
 function AddEventListenerFunc() {
   todoCreateForm.addEventListener("submit", CreateTask);
-  clearAllTodo.addEventListener("click",ClearAllList);
+  clearAllTodo.addEventListener("click", ClearAllList);
+
 }
 
 function AppendChildFunc() {
   document.body.appendChild(todoCreateForm);
   todoCreateForm.appendChild(inputText);
-  
   todoCreateForm.appendChild(createToDo);
   todoCreateForm.appendChild(clearAllTodo);
   todoCreateForm.appendChild(toDoListCardParent);
-
-  
 
   inputText.maxLength = "20";
 }

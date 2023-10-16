e110bfca598a172ab2610cf5fd64963c319c8972
atakanhbk <atakanhbk@gmail.com>
2023-10-16 10:51:03 +0300

    Changed Topic

--- app.js
+++ app.js
@@ -1,82 +1,42 @@
-let blueButton = document.createElement("button");
-let greenButton = document.createElement("button");
-let redButton = document.createElement("button");
-let allSquare = document.createElement("div");
+let inputText = document.createElement("input");
+let showInputText = document.createElement("button");
+let toDoListCardParent = document.createElement("div");
+
+function CreateTask() {
+  let toDoListCard = document.createElement("div");
+  toDoListCardParent.appendChild(toDoListCard);
+  toDoListCard.style.width = "100px";
+  toDoListCard.style.height = "100px";
+  toDoListCard.style.border = "2px solid black";
+  toDoListCard.style.margin = "10px";
+  toDoListCard.innerHTML = "ATAKAN";
+  toDoListCard.style.color = "white";
+  toDoListCard.style.display = "flex";
+  toDoListCard.style.alignItems = "center";
+  toDoListCard.style.justifyContent = "center";
+  toDoListCard.style.backgroundColor = "red";
+}
 
 function ObjectStyleControl() {
-  blueButton.innerHTML = "Create Blue Button";
-  blueButton.style.margin = "5px";
-  blueButton.style.borderRadius = "5px";
-  blueButton.style.backgroundColor = "blue";
-  blueButton.style.color = "white";
+  showInputText.innerHTML = "Show Input";
 
-  greenButton.innerHTML = "Create Green Button";
-  greenButton.style.margin = "5px";
-  greenButton.style.borderRadius = "5px";
-  greenButton.style.backgroundColor = "green";
-  greenButton.style.color = "white";
+  toDoListCardParent.style.display = "flex";
+  toDoListCardParent.style.flexWrap = "wrap";
+  toDoListCardParent.style.gap = "10px";
+}
 
-  redButton.innerHTML = "Create Red Button";
-  redButton.style.margin = "5px";
-  redButton.style.borderRadius = "5px";
-  redButton.style.backgroundColor = "red";
-  redButton.style.color = "white";
+function AddEventListenerFunc() {
+  showInputText.addEventListener("click", function () {
+    CreateTask();
+  });
+}
 
-  allSquare.style.padding = "10px";
-  allSquare.className = "all-square";
-  allSquare.style.display = "flex";
-  allSquare.style.flexWrap = "wrap";
-  allSquare.style.gap = "7px";
+function AppendChildFunc() {
+  document.body.appendChild(inputText);
+  document.body.appendChild(showInputText);
+  document.body.appendChild(toDoListCardParent);
 }
 
 ObjectStyleControl();
-
-function CreateSquare(color) {
-  switch (color) {
-    case "blue":
-      let blueSquare = document.createElement("div");
-
-      blueSquare.style.width = "100px";
-      blueSquare.style.height = "100px";
-      blueSquare.style.backgroundColor = "blue";
-
-      allSquare.appendChild(blueSquare);
-      break;
-    case "green":
-      let greenSquare = document.createElement("div");
-
-      greenSquare.style.width = "100px";
-      greenSquare.style.height = "100px";
-      greenSquare.style.backgroundColor = "green";
-
-      allSquare.appendChild(greenSquare);
-      break;
-
-    case "red":
-      let redSquare = document.createElement("div");
-
-      redSquare.style.width = "100px";
-      redSquare.style.height = "100px";
-      redSquare.style.backgroundColor = "red";
-
-      allSquare.appendChild(redSquare);
-      break;
-  }
-}
-
-blueButton.addEventListener("click", function () {
-  CreateSquare("blue");
-});
-
-greenButton.addEventListener("click", function () {
-  CreateSquare("green");
-});
-
-redButton.addEventListener("click", function () {
-  CreateSquare("red");
-});
-
-document.body.appendChild(blueButton);
-document.body.appendChild(greenButton);
-document.body.appendChild(redButton);
-document.body.appendChild(allSquare);
+AddEventListenerFunc();
+AppendChildFunc();

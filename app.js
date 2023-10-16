93966920f47b2d672349450b437715e0a0c98c10
atakanhbk <atakanhbk@gmail.com>
2023-10-16 12:01:02 +0300

    Clear All Added

--- app.js
+++ app.js
@@ -1,49 +1,87 @@
 let todoCreateForm = document.createElement("form");
 let inputText = document.createElement("input");
-let showInputText = document.createElement("button");
+let createToDo = document.createElement("button");
 let toDoListCardParent = document.createElement("div");
+let clearAllTodo = document.createElement("button");
 
-function CreateTask() {
-  let toDoListCard = document.createElement("div");
-  toDoListCardParent.appendChild(toDoListCard);
-  toDoListCard.style.width = "100px";
-  toDoListCard.style.height = "100px";
-  toDoListCard.style.border = "2px solid black";
-  toDoListCard.style.margin = "10px";
-  toDoListCard.innerHTML = `${inputText.value}`;
-  toDoListCard.style.color = "white";
-  toDoListCard.style.display = "flex";
-  toDoListCard.style.alignItems = "center";
-  toDoListCard.style.justifyContent = "center";
-  toDoListCard.style.backgroundColor = "red";
-}
+let toDoListCardList = [];
 
 function ObjectStyleControl() {
-  showInputText.innerHTML = "Show Input";
+  createToDo.innerHTML = "Show Input";
 
   toDoListCardParent.style.display = "flex";
   toDoListCardParent.style.flexWrap = "wrap";
   toDoListCardParent.style.gap = "10px";
+  toDoListCardParent.className="todo-parent";
+
+  createToDo.type = "submit";
+
+  clearAllTodo.innerHTML = "Clear All Todo";
+  clearAllTodo.style.backgroundColor = "red";
+  clearAllTodo.type = "button"
 }
 
-function OnSubmitFunction(e) {
+function CreateTask(e) {
   e.preventDefault();
-  console.log("On submit");
+
+  if (inputText.value === "") {
+    alert("Please Enter Any Value");
+  } else {
+    let toDoListCard = document.createElement("div");
+    let toDoListCardText = document.createElement("span");
+
+    toDoListCardParent.appendChild(toDoListCard);
+    ToDoListCardStyle(toDoListCard);
+
+    toDoListCard.appendChild(toDoListCardText);
+    toDoListCardList.push(inputText.value);
+    console.log(toDoListCardList);
+    inputText.value = "";
+  }
 }
+
+function ClearAllList() {
+  while (toDoListCardParent.firstChild) {
+    toDoListCardParent.removeChild(toDoListCardParent.lastChild);
+    toDoListCardList = [];
+  }
+}
+
+function ToDoListCardStyle(card) {
+  const cardStyle = card.style;
+  cardStyle.width = "100px";
+  cardStyle.height = "100px";
+  cardStyle.border = "2px solid black";
+  cardStyle.margin = "10px";
+  card.innerHTML = `${inputText.value}`;
+  cardStyle.color = "white";
+  cardStyle.display = "flex";
+  cardStyle.alignItems = "center";
+  cardStyle.justifyContent = "center";
+  cardStyle.backgroundColor = "white";
+  cardStyle.color = "black";
+  cardStyle.wordBreak =  "break-word"; 
+  cardStyle.overflow = "hidden";
+  cardStyle.padding = "10px";
+}
+
 
 function AddEventListenerFunc() {
-  showInputText.addEventListener("click", function () {
-    CreateTask();
-  });
-
-  todoCreateForm.addEventListener("submit",OnSubmitFunction)
+  todoCreateForm.addEventListener("submit", CreateTask);
+  clearAllTodo.addEventListener("click",ClearAllList);
 }
 
 function AppendChildFunc() {
   document.body.appendChild(todoCreateForm);
   todoCreateForm.appendChild(inputText);
-  todoCreateForm.appendChild(showInputText);
+  
+  todoCreateForm.appendChild(createToDo);
+  todoCreateForm.appendChild(clearAllTodo);
   todoCreateForm.appendChild(toDoListCardParent);
+
+  
+
+  inputText.maxLength = "20";
 }
 
 ObjectStyleControl();
